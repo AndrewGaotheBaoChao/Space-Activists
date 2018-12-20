9b8dfe005ebc547796a65f6617b74095803afefa
Jagraj Aulakh <aulakhjagraj@outlook.com>
2018-12-20 15:38:27 -0500

    Basic player walking animations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pygame import *
 from math import *
 from random import *
+import images
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
@@ -9,35 +10,95 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 init()
+images.load()
+width, height = 1280, 720
+screen = display.set_mode((width, height))
+
 clock = time.Clock() # FPS Clock
-currentScreen = "Menu" # Keeps track of which screen the user is on
+currentScreen = "game" # Keeps track of which screen the user is on
 running = True
+tick = 0
 
 class Game:
-	def init(self):
-		global window, width, height, title
-		width, height = 1280, 720
-		title = "English Project"
-		self.screen = display.set_mode((width, height))
+	def __init__(self):
+		self.title = "English Project"
+		display.set_caption(self.title)
+		self.player = Player()
 
-	def start_screen(self):
-		self.screen.blit(menu, (0, 0))
-		self.screen.blit(newGameB, (389, 349))
-		self.screen.blit(helpB, (389, 483))
-		self.screen.blit(creditsB, (389, 550))
-		display.flip()
+		self.offset = [0, 0]
 
-	def start_game(self):
+	def update_menu(self):
 		pass
 
-	def events(self):
-		pass
+	def draw_menu(self):
+		screen.fill((50,50,50))
+		# screen.blit(menu, (0, 0))
+		# screen.blit(newGameB, (389, 349))
+		# screen.blit(helpB, (389, 483))
+		# screen.blit(creditsB, (389, 550))
+
+	def update_game(self):
+		self.player.update()
+
+	def draw_game(self):
+		screen.fill((50,50,50))
+		self.player.draw()
+
+class Player:
+	def __init__(self):
+		self.index = 0 # keeps track of what image to blit in animation
+		self.image = images.playerImages[self.index] # holds actual image for animation
+		self.rect = self.image.get_rect()
+		self.rect.center = width/2, height/2
+		self.x, self.y = self.rect.midbottom
+		self.dir = 'down'
+
+		self.vx, self.vy = 0, 0
+
+	def determine_image(self):
+		if self.dir in ['up', 'down']:
+			if self.vy > 0:
+				self.index = 0
+			elif self.vy < 0:
+				self.index = 4
+		elif self.dir in ['right', 'left']:
+			if self.vx > 0:
+				self.index = 12
+			elif self.vx < 0:
+				self.index = 8
+
+		if self.vx == self.vy == 0:
+			d = ['down','up','left','right']
+			i = d.index(self.dir) * 4
+			if i == 12: i+=1
+			self.image = images.playerImages[i]
+		else:
+			self.image = images.playerImages[self.index + (int(tick/5) % 4)]
+
+	def update(self):
+		self.vx , self.vy = 0, 0
+		kp = key.get_pressed()
+		if kp[K_RIGHT] or kp[K_d]:
+			self.dir = 'right'
+			self.vx = 5
+		if kp[K_LEFT] or kp[K_a]:
+			self.dir = 'left'
+			self.vx = -5
+		if kp[K_DOWN] or kp[K_s]:
+			self.dir = 'down'
+			self.vy = 5
+		if kp[K_UP] or kp[K_w]:
+			self.dir = 'up'
+			self.vy = -5
+
+		self.determine_image()
+
+	def draw(self):
+		screen.blit(self.image, self.rect)
 
 g = Game()
 
 while running:
-
-	clock.tick()  # Advances the clock for FPS
 	mx, my = mouse.get_pos()  # Mouse location
 	mb = mouse.get_pressed()  # Mouse click status
 	click = False  # Resets mouse click so that it only counts as one click
@@ -50,16 +111,21 @@ while running:
 				running = False
 
 		# If mouse button is released
-		if e.type == MOUSEBUTTONUP and e.button == 1:
-			click = True
-			print(click)
+		if evt.type == MOUSEBUTTONUP:
+			if evt.button == 1:
+				click = True
+				print(click)
 
-	if currentScreen == "Menu":
-		g.start_screen()
+	if currentScreen == "menu":
+		g.update_menu()
+		g.draw_menu()
 
-	elif currentScreen == "Game":
-		g.start_game()
+	elif currentScreen == "game":
+		g.update_game()
+		g.draw_game()
 
 	display.flip()
+	clock.tick(60) # FPS
+	tick = (tick + 1) % (60*120)
 
 quit()
