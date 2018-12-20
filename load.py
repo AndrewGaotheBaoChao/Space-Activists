ce9a79697345bec624da3e2160a45caaf4c260f3
Andrew Gao <aggaodrew@gmail.com>
2018-12-20 12:34:40 -0500

    updates

---
+++ load.py
@@ -0,0 +1,9 @@
+from pygame import *
+
+# Menu background and buttons images
+menu = image.load("img/menu.png")
+newGameB = image.load("img/newGame.png")
+helpB = image.load("img/help.png")
+creditsB = image.load("img/credits.png")
+
+# Menu button rects
--- main.py
+++ main.py
@@ -9,31 +9,56 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
 init()
-
-width, height = 960, 540
-screen = display.set_mode((width, height))
+clock = time.Clock() # FPS Clock
+currentScreen = "Menu" # Keeps track of which screen the user is on
 running = True
 
+class Game:
+	def init(self):
+		global window, width, height, title
+		width, height = 1280, 720
+		title = "English Project"
+		self.screen = display.set_mode((width, height))
+
+	def start_screen(self):
+		self.screen.blit(menu, (0, 0))
+		self.screen.blit(newGameB, (389, 349))
+		self.screen.blit(helpB, (389, 483))
+		self.screen.blit(creditsB, (389, 550))
+		display.flip()
+
+	def start_game(self):
+		pass
+
+	def events(self):
+		pass
+
+g = Game()
+
 while running:
+
+	clock.tick()  # Advances the clock for FPS
+	mx, my = mouse.get_pos()  # Mouse location
+	mb = mouse.get_pressed()  # Mouse click status
+	click = False  # Resets mouse click so that it only counts as one click
+
 	for evt in event.get():
 		if evt.type == QUIT:
 			running = False
 		elif evt.type == KEYDOWN:
 			if evt.key == K_ESCAPE:
 				running = False
-				
-		elif evt.type == MOUSEBUTTONDOWN:
-			if evt.button == 1: # LEFT CLICK
-				pass
 
-		elif evt.type == MOUSEBUTTONUP:
-			if evt.button == 1: # LEFT CLICK
-				pass
+		# If mouse button is released
+		if e.type == MOUSEBUTTONUP and e.button == 1:
+			click = True
+			print(click)
 
-	mx, my = mouse.get_pos()
-	mb = mouse.get_pressed()
+	if currentScreen == "Menu":
+		g.start_screen()
 
-	screen.fill(WHITE)
+	elif currentScreen == "Game":
+		g.start_game()
 
 	display.flip()
 
