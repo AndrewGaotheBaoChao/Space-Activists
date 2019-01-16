854cd5c90ad2f6ce727cd3e7eb0fe6b60dd1a0ac
AndrewGaotheBaoChao <aggaodrew@gmail.com>
2019-01-15 21:38:46 -0500

    update

---
+++ images/4th_gen_indoor_tileset_by_akizakura16_dac0c2w.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="4th_gen_indoor_tileset_by_akizakura16_dac0c2w" tilewidth="32" tileheight="32" tilecount="4264" columns="8">
+ <image source="C:/Users/aggao/Desktop/4th_gen_indoor_tileset_by_akizakura16_dac0c2w.png" width="256" height="17056"/>
+</tileset>
